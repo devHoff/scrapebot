7ba89d984c21bb5f6b9b25cf4da18f9f0219fc76
Arthur Hoffmann <arthur.renck3@gmail.com>
2026-02-02 16:08:12 +0000

    Function to search cars

--- scrape.py
+++ scrape.py
@@ -1,2 +1,17 @@
-print("Hello world!")
-print("Testing")
+import requests
+
+URL = "https://django-anuncios.solyd.com.br/automoveis/"
+
+def search_cars(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(response.text)
+            print("Response status code:", response.status_code)
+        else:
+            print("Error when requesting")
+    except Exception as error:
+        print("Error when requesting")
+        print(error)
+
+search_cars(URL)
